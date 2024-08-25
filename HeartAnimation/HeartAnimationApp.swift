8b6266697f13b92cfd9717d47f42c688c9dc3209
Víctor Barrios <sygmaz19@gmail.com>
2024-08-25 23:56:24 +0200

    Initial Commit

---
+++ HeartAnimation/HeartAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartAnimationApp.swift
+//  HeartAnimation
+//
+//  Created by Víctor Barrios Sánchez on 25/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HeartAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
