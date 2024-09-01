52ae95016db0fa3a71068451d0f7a73bea09995c
Víctor Barrios <sygmaz19@gmail.com>
2024-09-01 19:52:35 +0200

    Place only one heart

--- HeartAnimation/ContentView.swift
+++ HeartAnimation/ContentView.swift
@@ -3,36 +3,28 @@ import SwiftUI
 struct ContentView: View {
     @State private var isFavourite = false
     
-    private let fonts = [
-        Font.body, 
-        Font.largeTitle,
-        Font.system(size: 72)
-    ]
-    
     var body: some View {
         VStack(spacing: 60) {
-            ForEach(fonts, id: \.self) {
-                Button(action: {
-                    isFavourite.toggle()
-                },
-                       label: {
-                    if isFavourite {
-                        Image(systemName: "heart")
-                            .foregroundStyle(.rainbowAngularGradient())
-                            .symbolVariant(.fill)
-                            .transition(
-                                .confetti(
-                                    color: .rainbowAngularGradient(),
-                                    size: 3
-                                )
+            Button(action: {
+                isFavourite.toggle()
+            },
+                   label: {
+                if isFavourite {
+                    Image(systemName: "heart")
+                        .foregroundStyle(.rainbowAngularGradient())
+                        .symbolVariant(.fill)
+                        .transition(
+                            .confetti(
+                                color: .rainbowAngularGradient(),
+                                size: 3
                             )
-                    } else {
-                        Image(systemName: "heart")
-                            .foregroundStyle(.gray)
-                    }
-                })
-                .font($0)
-            }
+                        )
+                } else {
+                    Image(systemName: "heart")
+                        .foregroundStyle(.gray)
+                }
+            })
+            .font(.system(size: 92))
         }
         .padding()
     }
