7b689c44dd8d27198b3681a7b91f49d534c712ff
Víctor Barrios <sygmaz19@gmail.com>
2024-09-02 06:50:20 +0200

    Add rainbow mode to screen

--- HeartAnimation/ContentView.swift
+++ HeartAnimation/ContentView.swift
@@ -2,10 +2,28 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var isFavourite = false
+    @State private var enableRainbowMode = false
     
     var body: some View {
-        VStack(spacing: 60) {
+        ZStack {
             heartButton
+            
+            VStack {
+                Spacer()
+                
+                Toggle(isOn: $enableRainbowMode, label: {
+                    HStack {
+                        Image(systemName: "rainbow")
+                            .foregroundStyle(.rainbowAngularGradient())
+                        
+                        Text("Enable rainbow mode?")
+                            .monospaced()
+                            .fontDesign(.monospaced)
+                    }
+                    .font(.headline)
+                })
+            }
+            .padding()
         }
         .padding()
     }
@@ -17,15 +35,27 @@ struct ContentView: View {
             },
             label: {
                 if isFavourite {
-                    Image(systemName: "heart")
-                        .foregroundStyle(.orange.gradient)
-                        .symbolVariant(.fill)
-                        .transition(
-                            .confetti(
-                                color: .orange.gradient,
-                                size: 9
+                    if enableRainbowMode {
+                        Image(systemName: "heart")
+                            .foregroundStyle(.rainbowAngularGradient())
+                            .symbolVariant(.fill)
+                            .transition(
+                                .confetti(
+                                    color: .rainbowAngularGradient(),
+                                    size: 9
+                                )
                             )
-                        )
+                    } else {
+                        Image(systemName: "heart")
+                            .foregroundStyle(.orange.gradient)
+                            .symbolVariant(.fill)
+                            .transition(
+                                .confetti(
+                                    color: .orange.gradient,
+                                    size: 9
+                                )
+                            )
+                    }
                 } else {
                     Image(systemName: "heart")
                         .foregroundStyle(.gray)
